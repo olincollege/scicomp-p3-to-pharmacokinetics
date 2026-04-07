bada70b82ee75e14f7b2e8abc2b4b7e784023855
Titilayo Oshinowo <titiooshy@gmail.com>
2026-04-07 13:33:17 -0400

    Initial added constants section of code

---
+++ constants.py
@@ -0,0 +1,18 @@
+#constants.py section 
+# will use this section primarily for all the parameters mentioned in the papers
+
+
+# Parameters for the Pharmacokinetic (from paper)
+CL = 0.22   # L/hr/kg
+Vc = 0.30   # L/kg
+Vp = 10.0   # L/kg
+Q  = 0.71   # L/hr/kg
+
+# Simulation settings
+TIME_START = 0
+TIME_END = 48
+NUM_POINTS = 200
+
+# Initial conditions
+INITIAL_CENTRAL = 10
+INITIAL_PERIPHERAL = 0
