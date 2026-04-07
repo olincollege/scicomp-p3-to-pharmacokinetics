6c0854d432b374d62fe4534542e1e53afc0521be
Titilayo Oshinowo <titiooshy@gmail.com>
2026-04-07 14:07:05 -0400

    Initial model.py implementation of the compartmental models

---
+++ model.py
@@ -0,0 +1,41 @@
+"""
+model.py
+"""
+
+def first_compartment(t, C, CL, V):
+    """
+    The first compartment of the model for just one case. 
+
+    Args:
+        t (float): time measured in hours
+        C (float): the drug concentration in central compartment
+        Cl (float): the clearance being measured 
+        V (float): the measured volume of the distribution 
+
+    Returns:
+        The rate of of change of the concentration that we are measuring 
+    """
+    return -(CL / V) * C
+
+def second_compartment(t, y, CL, Vc, Vp, Q):
+    """
+    The second comparment model for when there are two cases.
+
+    Args:
+        t (float): time measured in hours 
+        y (list): a list containing [Cc, Cp] with Cc = central compartment concentration
+                  Cp = peripheral compartment concentration
+        Cl (float): the clearance
+        Vc (float): the measured central volume
+        Vp (float): the measured perceptral volume 
+        Q (float): the inter-compartmental volume that is measured 
+    
+    Returns:
+        An array 
+    """
+    Cc, Cp = y
+
+    dCc_dt = -(CL / Vc) * Cc - (Q / Vc) * (Cc - Cp)
+    dCp_dt = (Q / Vp) * (Cc - Cp)
+
+    return [dCc_dt, dCp_dt]
