62795d078dec3b698140023b4fe3946104a3da66
Titilayo Oshinowo <titiooshy@gmail.com>
2026-04-11 16:22:27 -0400

    Added in the simulation for two comparements

--- simulation.py
+++ simulation.py
@@ -30,7 +30,7 @@ def run_first_compartment():
         None
 
     Returns:
-        A tuple with the timeppoints and concentration values
+        A tuple with the time points and concentration values
     """
     t_eval = np.linspace(TIME_START, TIME_END, NUM_POINTS)
 
@@ -43,3 +43,31 @@ def run_first_compartment():
     )
 
     return solution.t, solution.y[0]
+
+
+def run_second_compartment():
+    """
+    The goal is to run the main two-compartment benchmark simulation. This essentially is the main
+    simulation code function that will be used to reproduce the published plasma concentration-time
+    curves.
+
+    Args:
+        None
+
+    Returns:
+        A tuple containing the time points, central concentration, peripheral concentration
+    """
+    t_eval = np.linspace(TIME_START, TIME_END, NUM_POINTS)
+
+    solution = solve_ivp(
+        second_compartment,
+        [TIME_START, TIME_END],
+        [INITIAL_CENTRAL, INITIAL_PERIPHERAL],
+        args=(CL, VC, VP, Q),
+        t_eval=t_eval,
+    )
+
+    central = solution.y[0]
+    peripheral = solution.y[1]
+
+    return solution.t, central, peripheral
