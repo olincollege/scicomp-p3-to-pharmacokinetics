ac3ca1f53e10fb98ad233b3bf1b9149d658fca04
Titilayo Oshinowo <titiooshy@gmail.com>
2026-04-19 10:29:04 -0400

    Changed the constants to account for more time taken and higher dosage amount

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,7 @@
 constants.py
 
 This file stores all of the pharmacokinetic constants and simulation defaults used across
-the the papers that we reading and benchmarking off of: Siddoway et al. (1996) and 
+the the papers that we reading and benchmarking off of: Siddoway et al. (1996) and
 Brodan et al. (1982) amiodarone papers.
 
 Keeping these values centralized makes it easier to:
@@ -14,26 +14,26 @@ Keeping these values centralized makes it easier to:
 
 # Published pharmacokinetic parameters based on the paper
 
-CL = 0.22   # Clearance from central compartment (L/hr/kg)
-Vc = 0.30   # Central compartment volume (L/kg)
-Vp = 10.0   # Peripheral compartment volume L/kg
-Q  = 0.71   # Inter-compartmental clearance L/hr/kg
+CL = 0.22  # Clearance from central compartment (L/hr/kg)
+Vc = 0.30  # Central compartment volume (L/kg)
+Vp = 10.0  # Peripheral compartment volume L/kg
+Q = 0.71  # Inter-compartmental clearance L/hr/kg
 
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 
 # Simulation settings
 
-TIME_START = 0      # Simulation start time (hours)
-TIME_END = 48       # Simulation end time (hours)
-NUM_POINTS = 200    # Number of output points in benchmark plots
+TIME_START = 0  # Simulation start time (hours)
+TIME_END = 200  # Simulation end time (hours)
+NUM_POINTS = 200  # Number of output points in benchmark plots
 
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 
 # Initial conditions
 
 # Initial drug amount in central compartment
 # Represents IV bolus dose used for benchmark reproduction
-INITIAL_CENTRAL = 10
+INITIAL_CENTRAL = 150
 
 # Initial drug amount in peripheral compartment
 INITIAL_PERIPHERAL = 0
